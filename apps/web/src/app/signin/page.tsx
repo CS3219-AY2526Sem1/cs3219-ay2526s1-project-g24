659f0a8d2df6a0fb61f8ec798d49f0e8ad8f6df4
Ryann <ryanngohenrui@gmail.com>
2025-10-22 16:38:12 +0800

    Update signin

--- apps/web/src/app/signin/page.tsx
+++ apps/web/src/app/signin/page.tsx
@@ -3,7 +3,7 @@
 import Link from "next/link";
 import Image from "next/image";
 import { useRouter } from "next/navigation";
-import { getGoogleSignInUrl } from "../../lib/user.service";
+import { getGoogleSignInUrl } from "../../lib/api/user.service";
 
 export default function SignIn() {
     const router = useRouter();
